397273d7aca05b56f6e1f8af01142cee25ae5b98
Laurens Van Houtven <_@lvh.io>
2016-01-05 23:27:54 -0600

    Kill failing skeleton test

--- test/kegan/core_test.clj
+++ test/kegan/core_test.clj
@@ -1,7 +1,1 @@
-(ns kegan.core-test
-  (:require [clojure.test :refer :all]
-            [kegan.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(ns kegan.core-test)
