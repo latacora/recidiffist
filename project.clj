303497edd7ea17b554b91b683bc355a51f7e5e6c
Laurens Van Houtven <_@lvh.io>
2016-01-07 11:24:48 -0800

    Add cljfmt

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[lein-cljfmt "0.3.0"]]
   :main ^:skip-aot kegan.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
