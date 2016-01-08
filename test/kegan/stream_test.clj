00c40b95b914ef3fdbccd56ae6796a086859715d
Laurens Van Houtven <_@lvh.io>
2016-01-08 15:06:01 -0800

    Simple diffing case

--- test/kegan/stream_test.clj
+++ test/kegan/stream_test.clj
@@ -7,4 +7,11 @@
   (are [elems diffs] (= (into [] s/pairwise-diffing elems)
                         diffs)
     []
-    []))
+    []
+
+    [{:a 1}
+     {:a 2}
+     {:a 3}]
+    [{:added [] :changed [[[:a] 1 2]] :removed []}
+     {:added [] :changed [[[:a] 2 3]] :removed []}
+     {:added [] :changed [] :removed [[[:a] 3]]}]))
