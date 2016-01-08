5d0cb772a5746ad619d6ecd022d7181bfae66161
Laurens Van Houtven <_@lvh.io>
2016-01-08 14:48:29 -0800

    Add some dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/RackSec/kegan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [aleph "0.4.1-SNAPSHOT"]
+                 [net.cgrand/xforms "0.1.0"]
+                 [com.taoensso/timbre "4.2.1-SNAPSHOT"]]
   :plugins [[lein-cljfmt "0.3.0"]]
   :main ^:skip-aot kegan.core
   :target-path "target/%s"
