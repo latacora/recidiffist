db85eb5aa7fef16b08e43b1d599f254757488cd6
Laurens Van Houtven <_@lvh.io>
2016-01-09 15:28:02 -0800

    Don't show xducer finalization artifacts

--- test/kegan/stream_test.clj
+++ test/kegan/stream_test.clj
@@ -9,22 +9,32 @@
 
 (def update-diffs
   [{:added [] :changed [[[:a] 1 2]] :removed []}
-   {:added [] :changed [[[:a] 2 3]] :removed []}
-   {:added [] :changed [] :removed [[[:a] 3]]}])
+   {:added [] :changed [[[:a] 2 3]] :removed []}])
 
 (def add-update-remove-elems
   [{:a 1}
-   {:a 2 :b 1}
+   {:a 2
+    :b 1}
    {:b 2}])
 
 (def add-update-remove-diffs
-  [{:added [[[:a] 1]], :changed [], :removed []}
-   {:added [[[:b] 1]], :changed [[[:a] 1 2]], :removed []}
+  [{:added [[[:b] 1]], :changed [[[:a] 1 2]], :removed []}
    {:added [], :changed [[[:b] 1 2]], :removed [[[:a] 2]]}])
 
 
 (deftest pairwise-diffing-tests
-  (are [elems diffs] (= (into [] s/pairwise-diffing elems)
+  (are [elems diffs] (= (butlast
+                         ;; last record is because of finalization
+                         (into [] s/pairwise-diffing elems))
+                        diffs)
+    []
+    []
+
+    update-elems
+    update-diffs
+
+    add-update-remove-elems
+    add-update-remove-diffs))
                         diffs)
     []
     []
