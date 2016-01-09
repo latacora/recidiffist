00bcd08a3f596adb09548e02284c1e3880e51d27
Laurens Van Houtven <_@lvh.io>
2016-01-09 15:20:20 -0800

    Add sample with add, update & remove

--- test/kegan/stream_test.clj
+++ test/kegan/stream_test.clj
@@ -12,6 +12,17 @@
    {:added [] :changed [[[:a] 2 3]] :removed []}
    {:added [] :changed [] :removed [[[:a] 3]]}])
 
+(def add-update-remove-elems
+  [{:a 1}
+   {:a 2 :b 1}
+   {:b 2}])
+
+(def add-update-remove-diffs
+  [{:added [[[:a] 1]], :changed [], :removed []}
+   {:added [[[:b] 1]], :changed [[[:a] 1 2]], :removed []}
+   {:added [], :changed [[[:b] 1 2]], :removed [[[:a] 2]]}])
+
+
 (deftest pairwise-diffing-tests
   (are [elems diffs] (= (into [] s/pairwise-diffing elems)
                         diffs)
