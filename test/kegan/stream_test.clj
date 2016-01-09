3bc2ac14d245e54a390167da1209419845b34e53
Laurens Van Houtven <_@lvh.io>
2016-01-09 14:07:33 -0800

    Extract sample

--- test/kegan/stream_test.clj
+++ test/kegan/stream_test.clj
@@ -2,6 +2,15 @@
   (:require
    [kegan.stream :as s]
    [clojure.test :as t :refer [deftest testing is are]]))
+(def update-elems
+  [{:a 1}
+   {:a 2}
+   {:a 3}])
+
+(def update-diffs
+  [{:added [] :changed [[[:a] 1 2]] :removed []}
+   {:added [] :changed [[[:a] 2 3]] :removed []}
+   {:added [] :changed [] :removed [[[:a] 3]]}])
 
 (deftest pairwise-diffing-tests
   (are [elems diffs] (= (into [] s/pairwise-diffing elems)
@@ -9,9 +18,5 @@
     []
     []
 
-    [{:a 1}
-     {:a 2}
-     {:a 3}]
-    [{:added [] :changed [[[:a] 1 2]] :removed []}
-     {:added [] :changed [[[:a] 2 3]] :removed []}
-     {:added [] :changed [] :removed [[[:a] 3]]}]))
+    update-elems
+    update-diffs))
