4ef5b6237e11923ffc8673c7c01af7ffb4e58a79
Laurens Van Houtven <_@lvh.io>
2016-01-13 18:05:57 -0600

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kegan "0.3.0"
+(defproject kegan "0.3.1-SNAPSHOT"
   :description "Immutable logs from changing values."
   :url "https://github.com/RackSec/kegan"
   :license {:name "Eclipse Public License"
