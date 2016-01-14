5ab062983800d42cdb8d2dd60ca40f915652a88a
Laurens Van Houtven <_@lvh.io>
2016-01-13 20:19:25 -0600

    These are actually all sets

--- test/kegan/diff_test.clj
+++ test/kegan/diff_test.clj
@@ -21,55 +21,55 @@
   (are [a b diff] (= (d/fancy-diff a b) diff)
     {}
     {}
-    {:added []
-     :changed []
-     :removed []}
+    {:added #{}
+     :changed #{}
+     :removed #{}}
 
     {:a 1}
     {}
-    {:added []
-     :changed []
-     :removed [[[:a] 1]]}
+    {:added #{}
+     :changed #{}
+     :removed #{[[:a] 1]}}
 
     {}
     {:a 1}
-    {:added [[[:a] 1]]
-     :changed []
-     :removed []}
+    {:added #{[[:a] 1]}
+     :changed #{}
+     :removed #{}}
 
     {:a 1}
     {:a 2}
-    {:added []
-     :changed [[[:a] 1 2]]
-     :removed []}
+    {:added #{}
+     :changed #{[[:a] 1 2]}
+     :removed #{}}
 
     {:a {:b 1}}
     {:a {:b 2}}
-    {:added []
-     :changed [[[:a :b] 1 2]]
-     :removed []}
+    {:added #{}
+     :changed #{[[:a :b] 1 2]}
+     :removed #{}}
 
     {:a {:b 1}
      :c 1}
     {:a {:b 2}
      :c 1}
-    {:added []
-     :changed [[[:a :b] 1 2]]
-     :removed []}
+    {:added #{}
+     :changed #{[[:a :b] 1 2]}
+     :removed #{}}
 
     {:a {:b 1}}
     {:a {:b 1}
      :c {:d 1}}
-    {:added [[[:c :d] 1]]
-     :changed []
-     :removed []}
+    {:added #{[[:c :d] 1]}
+     :changed #{}
+     :removed #{}}
 
     {:a {:b 1}
      :c {:d 1}}
     {:a {:b 1}}
-    {:added []
-     :changed []
-     :removed [[[:c :d] 1]]}
+    {:added #{}
+     :changed #{}
+     :removed #{[[:c :d] 1]}}
 
     {:a {:b 1}
      :c {:d 1}
@@ -77,9 +77,9 @@
     {:a {:b 1}
      :e {:f 1}
      :x 2}
-    {:added [[[:e :f] 1]]
-     :changed [[[:x] 1 2]]
-     :removed [[[:c :d] 1]]}
+    {:added #{[[:e :f] 1]}
+     :changed #{[[:x] 1 2]}
+     :removed #{[[:c :d] 1]}}
 
     {:a {:b 1
          :c 1}
@@ -89,7 +89,7 @@
          :c 2}
      :i {:j 1}
      :x 2}
-    {:added [[[:i :j] 1]]
-     :changed [[[:x] 1 2]
-               [[:a :c] 1 2]]
-     :removed [[[:p :q] 1]]}))
+    {:added #{[[:i :j] 1]}
+     :changed #{[[:x] 1 2]
+                [[:a :c] 1 2]}
+     :removed #{[[:p :q] 1]}}))
