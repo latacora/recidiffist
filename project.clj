cb51013200c7a1679b73cce097bf341b2931d3a6
Laurens Van Houtven (lvh) <_@lvh.io>
2018-12-26 10:13:28 -0600

    Update profiles

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
                  [net.cgrand/xforms "0.19.0"
                   :exclusions [org.clojure/clojurescript]]
                  [com.taoensso/timbre "4.10.0"]]
-  :plugins [[lein-cljfmt "0.3.0"]
-            [lein-cloverage "1.0.7"]]
+  :plugins [[lein-cljfmt "0.6.3"]
+            [lein-cloverage "1.0.13"]]
   :main ^:skip-aot recidiffist.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
